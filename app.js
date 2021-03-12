bb7d7f053c015c2adcca91b9f898bba02f44fa1f
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2021-03-12 12:27:44 -0600

    added my funny fact

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server =
     (req,res)=> {               //Anonymous function to handle the request
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/plain') //Set the mime type
-      res.end('Hello World')    //Close the response and provide content
+      res.end('If I won the lottery,the first thing I will pay tution fee')    //Close the response and provide content
     }                           //No return needed, we modified the res object we got
 )
 
